bf59821e633b6d8eb506643c93bf8c3bcc498ffb
Priya <laxmipriyapadhi71@gmail.com>
2023-05-25 10:39:20 +0530

    second

--- src/test/java/DemoGit.java
+++ src/test/java/DemoGit.java
@@ -3,5 +3,13 @@ public class DemoGit {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+    public void Do()
+    {
+        System.out.println("do");
+    }
+    public void Work()
+    {
+        System.out.println("Work");
+    }
 
 }
